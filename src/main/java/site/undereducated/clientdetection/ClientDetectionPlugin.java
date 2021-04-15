24ea57cc7318ed259eb418ccd06bcc81e114de6f
undereducated <undereducate@protonmail.com>
2021-04-15 18:33:44 -0400

    Initial release

--- src/main/java/site/undereducated/clientdetection/ClientDetectionPlugin.java
+++ src/main/java/site/undereducated/clientdetection/ClientDetectionPlugin.java
@@ -2,7 +2,9 @@ package site.undereducated.clientdetection;
 
 import com.iridium.iridiumcolorapi.IridiumColorAPI;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import site.undereducated.clientdetection.commands.client;
 import site.undereducated.undereducatedutil.UndereducatedAPI;
@@ -11,16 +13,23 @@ import us.myles.ViaVersion.api.ViaAPI;
 
 public final class ClientDetectionPlugin extends JavaPlugin implements Listener {
 	private static ViaAPI viaAPI;
-	public static String prefix = ChatColor.translateAlternateColorCodes('&', IridiumColorAPI.process("<GRADIENT:586ba4>ClientDetection</GRADIENT:324376>") + " &8l&r ");
+	public static String prefix = "";
 	@Override
 	public void onEnable(){
 		this.viaAPI = Via.getAPI();
-		this.getCommand("client").setExecutor(new client());
+		this.getCommand("client").setExecutor(new client(this));
 		UndereducatedAPI.loadPlugin("ClientDetection");
+		// load config
+		getConfig().options().copyDefaults();
+		saveDefaultConfig();
+		prefix = ChatColor.translateAlternateColorCodes('&', IridiumColorAPI.process(this.getConfig().getString("prefix")));
 	}
+
 	@Override
 	public void onDisable() {
 		// Plugin shutdown logic
+		UndereducatedAPI.log("Shutting down...", "ClientDetection");
 	}
+
 	public static ViaAPI getViaAPI() { return viaAPI; }
 }
--- src/main/java/site/undereducated/clientdetection/commands/client.java
+++ src/main/java/site/undereducated/clientdetection/commands/client.java
@@ -1,16 +1,29 @@
 package site.undereducated.clientdetection.commands;
 
+import com.iridium.iridiumcolorapi.IridiumColorAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import site.undereducated.clientdetection.ClientDetectionPlugin;
+import site.undereducated.undereducatedutil.UndereducatedAPI;
+import site.undereducated.undereducatedutil.UndereducatedStatistics;
 import us.myles.ViaVersion.api.ViaAPI;
 import us.myles.ViaVersion.api.protocol.ProtocolVersion;
 
 public class client implements CommandExecutor {
+
+	Plugin plugin;
+	public client(ClientDetectionPlugin instance) {
+		plugin = instance;
+	}
+
+	static Plugin client = Bukkit.getPluginManager().getPlugin("ClientDetection");
+	public static String prefix = ChatColor.translateAlternateColorCodes('&', IridiumColorAPI.process(client.getConfig().getString("prefix")));
 
 	// Parse Client brand, and return prettified version
 	public String parseBrand(String brand){
@@ -32,29 +45,44 @@ public class client implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
 		final ViaAPI viaAPI = ClientDetectionPlugin.getViaAPI();
 		Player player = (Player) sender;
 		if(args.length >= 1){
+			// if the person cannot view another players client, send an error
 			if(!player.hasPermission("clientdetection.viewother")){
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', ClientDetectionPlugin.prefix + "&c You do not have permission to view other peoples client version!"));
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&c You do not have permission to view other peoples client version!"));
 				return true;
 			}
+			// if the argument equals reload, reload the client.
+			if(args[0].equals("reload")){
+				plugin.reloadConfig();
+				prefix = ChatColor.translateAlternateColorCodes('&', IridiumColorAPI.process(client.getConfig().getString("prefix")));
+				System.out.println(prefix);
+				UndereducatedAPI.log("Reloaded configuration", "clientdetection");
+				return true;
+			}
+			// get the target
 			Player target = Bukkit.getPlayer(args[0]);
+			// if target exists
 			if(target != null) {
 				final int playerVersion = viaAPI.getPlayerVersion(target.getUniqueId());
 				final String version = String.format("%s &7(&c%s&7)", ProtocolVersion.getProtocol(playerVersion).getName(), parseBrand(target.getClientBrandName()));
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', ClientDetectionPlugin.prefix + "&c"  + target.getName() + "&r&7 is on &c" + version));
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&c"  + target.getName() + "&r&7 is on &c" + version));
 				return true;
 			}
+			// else if there's no arguments
 		} else if(args.length < 1){
+			// if the player does not have permission
 			if(!player.hasPermission("clientdetection.use")){
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', ClientDetectionPlugin.prefix + "&c You do not have permission to view your client version!"));
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&c You do not have permission to view your client version!"));
 				return true;
 			}
+			// if sender is not null
 			if (sender != null) {
 				final int playerVersion = viaAPI.getPlayerVersion(player.getUniqueId());
 				final String version = String.format("&c%s &7(&c%s&7)", ProtocolVersion.getProtocol(playerVersion).getName(), parseBrand(player.getClientBrandName()));
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', ClientDetectionPlugin.prefix + "&c" + player.getName() + "&r&7 is on &c" + version));
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + "&c" + player.getName() + "&r&7 is on &c" + version));
 				return true;
 			}
 		}
